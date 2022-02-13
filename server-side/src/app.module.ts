dfccac3c95c13b0680e135353270761f5e958f5e
kuns019 <56548175+kuns019@users.noreply.github.com>
2022-02-13 17:08:42 +0700

    Add backend
    add member crud function and family main page api

--- server-side/src/app.module.ts
+++ server-side/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MemberModule } from './member/member.module';
+import { FamilyModule } from './family/family.module';
 
 @Module({
-  imports: [],
+  imports: [MemberModule, FamilyModule],
   controllers: [AppController],
   providers: [AppService],
 })
