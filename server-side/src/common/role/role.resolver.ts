d662e0ddba98cf1b885289b5db2ecea832c49d84
kuns019 <56548175+kuns019@users.noreply.github.com>
2022-02-20 17:29:00 +0700

    <Add> role to backend
    Add Query Role to Backend

--- server-side/src/common/role/role.resolver.ts
+++ server-side/src/common/role/role.resolver.ts
@@ -1,4 +1,45 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { RoleCreateInput } from './dto/create-role.input';
+import { RoleType } from './entities/role.entity';
+import { RoleService } from './role.service';
+import { Role } from './schemas/role.schema';
 
-@Resolver()
-export class RoleResolver {}
+@Resolver(of => RoleType)
+export class RoleResolver {
+
+    constructor(private readonly roleService: RoleService){ }
+
+    @Query(returns => [RoleType])
+    async roles(): Promise<RoleType[]> {
+        return this.roleService.findAll()
+    }
+
+    @Query(returns => [RoleType])
+    async findRoleByName(@Args('name') name: string): Promise<RoleType[]> {
+        return this.roleService.findByName(name)
+    }
+
+    @Query(returns => RoleType, { name: 'role' })
+    async findRoleById(@Args('id', { type: () => String } ) id:string): Promise<RoleType> {
+        return this.roleService.findById(id)
+    }
+
+    @Mutation(returns => RoleType)
+    async createRole(@Args('input') input: RoleCreateInput): Promise<RoleType> {
+        return this.roleService.create(input)
+    }
+
+    @Mutation(returns => RoleType)
+    async updateRole(
+        @Args('id') id: string,
+        @Args('input') input:RoleCreateInput
+    ) {
+        return this.roleService.update(id, input as Role)
+    }
+
+    @Mutation(returns => RoleType)
+    async deleteRole(@Args('id') id: string) {
+        return this.roleService.delete(id)
+    }
+
+}
--- server-side/src/common/role/role.service.ts
+++ server-side/src/common/role/role.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { RoleCreateInput } from './dto/create-role.input';
+import { RoleType } from './entities/role.entity';
+import { Role } from './schemas/role.schema';
 
 @Injectable()
-export class RoleService {}
+export class RoleService {
+
+    constructor(@InjectModel('role') private roleModel: Model<Role>) { }
+
+    async create(createRoleDto: RoleCreateInput): Promise<RoleType> {
+        const createRole = new this.roleModel(createRoleDto)
+        return await createRole.save()
+    }
+
+    async findAll(): Promise<RoleType[]> {
+        return await this.roleModel.find().exec()
+    }
+
+    async findById(id: string): Promise<RoleType> {
+        return await this.roleModel.findById({ _id: id })
+    }
+
+    async findByName(name: string): Promise<RoleType[]> {
+        return await this.roleModel.find({roleName: new RegExp(name, 'i') })
+    }
+
+    async delete(id: string): Promise<RoleType> {
+        return await this.roleModel.findByIdAndRemove(id);
+    }
+
+    async update(id: string, role: Role): Promise<RoleType> {
+        return await this.roleModel.findByIdAndUpdate(id, role, { new: true });
+    }
+
+}
