ca38fc80e1ba66a64124509ed088ccc7c185905f
kuns019 <56548175+kuns019@users.noreply.github.com>
2022-02-20 16:02:47 +0700

    <Upd> kickmember
    Update kick member function

--- client-side/pages/family/familyPageHost.vue
+++ client-side/pages/family/familyPageHost.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-app-bar color="#F90716" dense dark >
+    <v-app-bar color="#F90716" dense dark>
       <v-app-bar-nav-icon />
       <v-spacer />
       <v-btn icon>
@@ -32,32 +32,83 @@
       <v-card style="padding: 0px 20px 20px 20px">
         <v-row align="center" justify="center">
           <v-col cols="2">
-            <v-img style="width: 100%" src="https://cdn-icons-png.flaticon.com/512/1383/1383260.png" />
+            <v-img style="width: 100%" :src="serviceImage" />
           </v-col>
           <v-col cols="6">
             <v-row>
               <v-col style="">
                 <v-card-text> Youtube Premium </v-card-text>
-                <v-card-text style="font-size: 2rem; font-weight: bold;" v-if="!isEdit"> {{familyName}}</v-card-text>
-                <v-text-field :placeholder="familyName" filled v-else></v-text-field>
+                <v-card-text
+                  v-if="!isEdit"
+                  style="font-size: 2rem; font-weight: bold;"
+                >
+                  {{ familyName }}
+                </v-card-text>
+                <v-text-field
+                  v-else
+                  v-model="editFamiltName"
+                  :placeholder="familyName"
+                  filled
+                />
               </v-col>
               <v-col v-show="!isEdit">
                 <v-card-text> Progress Bar </v-card-text>
               </v-col>
             </v-row>
           </v-col>
-          <v-col cols="2" v-if="!isEdit">
-            <div style="background-color:#C5C5C5; border-radius: 25px; text-align: center; height: 80px;">BRUQ856 <br>
-              <v-btn color="black" class="title" style="border-radius: 15px; width: 100%; height: 35px; margin-top: 10%">Copy Code</v-btn>
+          <v-col v-if="!isEdit" cols="2">
+            <div
+              style="
+                background-color:#C5C5C5;
+                border-radius: 25px;
+                text-align: center;
+                height: 80px;"
+            > BRUQ856 <br>
+              <v-btn
+                color="black"
+                class="title"
+                style="
+                  border-radius: 15px;
+                  width: 100%;
+                  height: 35px;
+                  margin-top: 10%"
+              > Copy Code </v-btn>
             </div>
           </v-col>
-          <v-col cols="2" v-else></v-col>
+          <v-col v-else cols="2"/>
           <v-col cols="2">
-            <template v-if="!isEdit" >
-              <v-btn color="purple" class="title" style="border-radius: 15px; width: 100%; height: 40px; margin: 2%" @click="isEdit = !isEdit">Edit</v-btn>
-              <v-btn color="black" class="title" style="border-radius: 15px; width: 100%; height: 40px; margin: 2%">History</v-btn>
+            <template v-if="!isEdit">
+              <v-btn
+                color="purple"
+                class="title"
+                style="
+                  border-radius: 15px;
+                  width: 100%;
+                  height: 40px;
+                  margin: 2%"
+                @click="toggleEditAneSave"
+              > Edit </v-btn>
+              <v-btn
+                color="black"
+                class="title"
+                style="
+                  border-radius: 15px;
+                  width: 100%;
+                  height: 40px;
+                  margin: 2%"
+              > History </v-btn>
             </template>
-            <v-btn color="success" class="title" style="border-radius: 15px; width: 100%; height: 40px; margin: 2%" @click="isEdit = !isEdit" v-else>Done</v-btn>
+            <v-btn
+              v-else
+              color="success"
+              class="title"
+              style="
+                border-radius: 15px;
+                width: 100%;
+                height: 40px;
+                margin: 2%"
+              @click="toggleEditAneSave"
+            > Done </v-btn>
           </v-col>
         </v-row>
         <v-row>
@@ -73,52 +124,104 @@
             <span> จำนวนคนสูงสุด </span><br>
             <span style="font-size: 2rem;"> 6/6 คน </span>
           </v-col>
-          <v-col cols="3"></v-col>
+          <v-col cols="3" />
         </v-row>
         <v-row>
           <v-col cols="4" style="font-weight: bold">MEMBER</v-col>
           <v-col cols="3" style="font-weight: bold">EMAIL</v-col>
           <v-col cols="3" style="font-weight: bold">PHONE</v-col>
-          <v-col cols="1"></v-col>
-          <v-col cols="1"></v-col>
+          <v-col cols="1" />
+          <v-col cols="1" />
         </v-row>
         <!-- member 1-6 -->
-        <v-row align="center" justify="center" v-for="member in members" :key="member.id">
+        <v-row v-for="member in members" :key="member.id" align="center" justify="center">
           <v-col cols="4">
             <v-row align="center" justify="center">
-              <v-col cols="4"><v-img :src='member.image' width="50px"></v-img></v-col>
-              <v-col cols="8">{{member.name}}<span v-show="member.role === 'Host'"> (HOST)</span></v-col>
+              <v-col cols="4">
+                <v-img :src="member.image" width="50px" />
+              </v-col>
+              <v-col cols="8">{{ member.name }}
+                <span v-show="member.role === 'Host'"> (HOST)</span>
+              </v-col>
             </v-row>
           </v-col>
           <v-col cols="3">{{member.email}}</v-col>
           <v-col cols="2">{{member.phone}}</v-col>
           <template v-if="!isEdit">
             <v-col cols="2">
-              <v-btn depressed color="warning" v-if="member.status === 'waiting'"> Approve </v-btn>
-              <v-btn depressed disabled v-else> Approve </v-btn>
+              <v-btn
+                v-if="member.status === 'waiting'"
+                depressed
+                color="warning"
+              > Approve </v-btn>
+              <v-btn v-else depressed disabled> Approve </v-btn>
             </v-col>
             <v-col cols="1">
-              <v-btn style="width: 100%" color="success" v-if="member.status === 'paid'"> Paid </v-btn>
-              <v-btn style="width: 100%" color="warning" v-if="member.status === 'waiting'"> Waiting </v-btn>
-              <v-btn style="width: 100%" color="error" v-if="member.status === 'not paid'"> Not Paid </v-btn>
+              <v-btn
+                v-if="member.status === 'paid'"
+                style="width: 100%"
+                color="success"
+              > Paid </v-btn>
+              <v-btn
+                v-if="member.status === 'waiting'"
+                style="width: 100%"
+                color="warning"
+              > Waiting </v-btn>
+              <v-btn
+                v-if="member.status === 'not paid'"
+                style="width: 100%"
+                color="error"
+              > Not Paid </v-btn>
             </v-col>
           </template>
           <template v-else>
-            <v-col cols="2"></v-col>
+            <v-col cols="2"/>
             <v-col cols="1">
-              <v-btn style="width: 100%" color="error" v-show="member.role !== 'Host'" @click='kickMember(member.id)'> Kick </v-btn>
+              <v-btn
+                v-show="member.role !== 'Host'"
+                style="width: 100%"
+                color="error"
+                @click="kickMemberModal(member)"
+              > Kick </v-btn>
             </v-col>
           </template>
         </v-row>
       </v-card>
     </v-row>
+    <!-- confirm delete -->
+    <div class="text-center">
+      <v-dialog v-model="kickModal" width="500">
+        <v-card>
+          <v-card-title class="text-h5 grey lighten-2">
+            Kick Member
+          </v-card-title>
+          <v-card-text style="padding: 5%">
+            Do you want to kick {{ tmp.name }} from your family
+          </v-card-text>
+          <v-divider />
+          <v-card-actions>
+            <v-spacer />
+            <v-btn text @click="kickModal = false">
+              Cancel
+            </v-btn>
+            <v-btn color="primary" text @click="kickMember(tmp.id)">
+              Confirm
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
   </div>
 </template>
 <script>
 export default {
   data: () => ({
     isEdit: false,
+    serviceImage: 'https://cdn-icons-png.flaticon.com/512/1383/1383260.png',
     familyName: 'Bruno Family',
+    editFamiltName: '',
+    kickModal: false,
+    tmp: {},
     rules: [
       value => !!value || 'Required.',
       value => (value && value.length >= 3) || 'Min 3 characters'
@@ -185,7 +288,19 @@ export default {
       this.members = this.members.filter((member) => {
         return member.id !== id
       })
+      this.kickModal = false
       // console.log(this.members)
+    },
+    toggleEditAneSave () {
+      this.isEdit = !this.isEdit
+      if (this.editFamiltName !== this.familyName && this.editFamiltName) {
+        this.familyName = this.editFamiltName
+      }
+      this.kickModal = false
+    },
+    kickMemberModal (member) {
+      this.tmp = member
+      this.kickModal = true
     }
   }
 }
