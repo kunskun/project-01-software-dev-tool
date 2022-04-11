a73f8bba5fee248a7abaa872a4201bec5370386b
Apisorn <62070218@it.kmitl.ac.th>
2022-04-11 16:47:02 +0700

    update paybill page
    update paybill page

---
+++ client-side/pages/family/paybill.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-app-bar color="#F90716" dense dark>
+      <v-app-bar-nav-icon />
+      <v-spacer />
+      <v-btn icon>
+        <v-icon color="info">
+          mdi-account-circle
+        </v-icon>
+      </v-btn>
+      <v-menu left bottom>
+        <template #activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon color="warning">
+              mdi-email
+            </v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+            <v-list-item-title>Notifications {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+</template>
