3785744817d4078c4cb85f25cb833a3f43709589
sirapob62070197 <97669848+sirapob62070197@users.noreply.github.com>
2022-04-13 17:06:38 +0700

    <Upd>button_back

--- client-side/pages/family/upload.vue
+++ client-side/pages/family/upload.vue
@@ -1,6 +1,12 @@
 <template>
-    <v-card class="backgroud">
+<v-card class="backgroud">
         <center><h1 class="pay">PAY</h1></center>
+        <br>
+        <v-col class="btn-right">
+            <v-btn class="button" color ="primary" :to="url" large href="#">
+              Back
+            </v-btn>
+        </v-col>
     </v-card>
 </template>
 <style scoped>
