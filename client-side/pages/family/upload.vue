ab8d12372a007f316d8982794935600af23b1c05
sirapob62070197 <97669848+sirapob62070197@users.noreply.github.com>
2022-04-13 17:11:19 +0700

    <Upd>create_linkpage

--- client-side/pages/family/upload.vue
+++ client-side/pages/family/upload.vue
@@ -1,7 +1,18 @@
 <template>
-<v-card class="backgroud">
+    <v-card class="backgroud">
         <center><h1 class="pay">PAY</h1></center>
         <br>
+        <div class="row">
+            <div class="col-lg-3">
+                <center>
+                    <div class="line">
+                        <a class="circle" id="c1" @click="url" large href="#">1</a>
+                        <div class="circle" id="c2">2</div>
+                        <div class="triangle"></div>
+                    </div>
+                </center>
+            </div>
+            </div>
         <v-col class="btn-right">
             <v-btn class="button" color ="primary" :to="url" large href="#">
               Back
